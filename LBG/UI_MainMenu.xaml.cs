8256682ded0033fc802196d08cb74640d18c2d89
Maria E. Castagnola <Maria E. Castagnola@MCASTAGN>
2016-06-01 23:19:58 -0300

    IU Pantalla principal (sin implementacion)

--- LBG/UI_MainMenu.xaml.cs
+++ LBG/UI_MainMenu.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LBG
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UI_MainMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UI_MainMenu : Window
     {
-        public MainWindow()
+        public UI_MainMenu()
         {
             InitializeComponent();
         }
