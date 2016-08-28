e895ed1a099719615778e3e27ef5c6322dc0d27e
Maria E. Castagnola <Maria E. Castagnola@MCASTAGN>
2016-08-28 20:11:43 -0300

    Detectar mano en la cabeza -2

--- LBG/UI_GameThreePieces.xaml.cs
+++ LBG/UI_GameThreePieces.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
+
 
 namespace LBG
 {
@@ -23,6 +12,25 @@ namespace LBG
     public partial class UI_GameThreePieces : Window
     {
         static KinectSensorChooser miKinect;
+        const int PostureDetectionNumber = 10;
+        int accumulator = 0;
+        Posture postureInDetection = Posture.None;
+        Posture previousPosture = Posture.None;
+
+        [Serializable]
+        public struct Vector3
+        {
+            public float X;
+            public float Y;
+            public float Z;
+        }
+
+
+        public enum Posture
+        {
+            None,
+            HandOnHead
+        }
 
         public UI_GameThreePieces()
         {
@@ -32,9 +40,7 @@ namespace LBG
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             miKinect = new KinectSensorChooser();
-            miKinect.KinectChanged += miKinect_KinectChanged;
-            //detecta si un kinect se conecta o esta desconectado, etc...
-            // si lo desconectamos nos manda al evento
+            miKinect.KinectChanged += miKinect_KinectChanged; //detecta si un kinect se conecta o esta desconectado, etc... si lo desconectamos nos manda al evento
             sensorChooserUI.KinectSensorChooser = miKinect;
             miKinect.Start(); //inicializar el kinect
         }
@@ -61,18 +67,14 @@ namespace LBG
 
             try //habilitar la profundidad y esqueletos
             {
-                e.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                //asigno el formato a la imagen
+                e.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30); //asigno el formato a la imagen
                 e.NewSensor.SkeletonStream.Enable();
 
                 try
                 {
-                    e.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
-                    //este modo nos permite estar sentado pero detectar las articulaciones de la parte superior del cuerpo
-                    e.NewSensor.DepthStream.Range = DepthRange.Near;
-                    //para que tenga rango mas cercano solo para kinect windows
-                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
-                    //para el rango cercano de los esqueletos
+                    e.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated; //este modo nos permite estar sentado pero detectar las articulaciones de la parte superior del cuerpo
+                    e.NewSensor.DepthStream.Range = DepthRange.Near; //para que tenga rango mas cercano solo para kinect windows
+                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = true; //para el rango cercano de los esqueletos
                 }
                 catch (InvalidOperationException)
                 {
@@ -87,10 +89,80 @@ namespace LBG
 
             ZonaCursor.KinectSensor = e.NewSensor; //ya tenemos el cursor
         }
+
+        void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            foreach (SkeletonData skeleton in e.SkeletonFrame.Skeletons)
+                if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    Vector3 handRight = new Vector3();
+                    handRight.X = skeleton.Joints[JointType.HandRight].Position.X;
+                    handRight.Y = skeleton.Joints[JointType.HandRight].Position.Y;
+                    handRight.Z = skeleton.Joints[JointType.HandRight].Position.Z;
+
+                    Vector3 handLeft = new Vector3();
+                    handLeft.X = skeleton.Joints[JointType.HandLeft].Position.X;
+                    handLeft.Y = skeleton.Joints[JointType.HandLeft].Position.Y;
+                    handLeft.Z = skeleton.Joints[JointType.HandLeft].Position.Z;
+
+                    Vector3 head = new Vector3();
+                    head.X = skeleton.Joints[JointType.Head].Position.X;
+                    head.Y = skeleton.Joints[JointType.Head].Position.Y;
+                    head.Z = skeleton.Joints[JointType.Head].Position.Z;
+                }
+        }
+
+
+        bool HandOnHead(Vector3 hand, Vector3 head)
+        {
+            float distance = (hand.X - head.X) - (hand.Y - head.Y) - (hand.Z - head.Z);
+            if (Math.Abs(distance) > 0.05f)
+                return false;
+            else
+                return true;
+        }
+
+        bool PostureDetector(Posture posture)
+        {
+            if (postureInDetection != posture)
+            {
+                accumulator = 0;
+                postureInDetection = posture;
+                return false;
+            }
+            if (accumulator < PostureDetectionNumber)
+            {
+                accumulator++;
+                return false;
+            }
+            if (posture != previousPosture)
+            {
+                previousPosture = posture;
+                accumulator = 0;
+                return true;
+            }
+            else
+                accumulator = 0;
+            return false;
+        }
+
         private void btn_head(object sender, RoutedEventArgs e)
         {
            MessageBox.Show("CABEZA");
+
+            if (HandOnHead(handRight, head))
+            {
+                if (PostureDetector(Posture.HandOnHead))
+                {
+                    //
+                }
+            }
+            else if (PostureDetector(Posture.None))
+            {
+                //
+            }
         }
+    
 
         private void btn_torso(object sender, RoutedEventArgs e)
         {
